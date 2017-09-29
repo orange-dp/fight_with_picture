49896420cf58a3711590e226c0d9a95f03f66c36
duanpeng <duanpeng@teraspek.com>
2017-09-29 18:55:45 +0800

    [A] add mode choice and some sample command

--- auto-test-policer.py
+++ auto-test-policer.py
@@ -2,7 +2,7 @@
 from wxpy import *
 from wechat_sender import *
 import os
-import random
+import random 
 
 bot = Bot(cache_path=True)
 my_friend = bot.friends().search(u'苏裕')[0]
@@ -10,6 +10,15 @@ my_group = bot.groups().search(u'万安')[0]
 bot.file_helper.send('Hello WeChat!')
 myself = bot.self
 
+#when say "dpdp" start to work
+#when say "help" repaly what is the usage
+#when say "stop" stop fxxking
+#fun mode(2):when say "have fun" to change fun mode
+#linux shell mode(3):when say "work a moment" 
+#replay
+#
+
+SWITCH = 0;
 
 class MsgInfo:
     sender = u''
@@ -33,6 +42,7 @@ def parserfile(filename):
     return remsg
 
 def parsermsg(msg):
+    global SWITCH
     if(msg.type == RECORDING):
         msg.get_file("./Recoding/"+msg.file_name)
         return 
@@ -45,41 +55,63 @@ def parsermsg(msg):
         return 
     elif(msg.type == TEXT):
         info = MsgInfo(msg)
-        if(msg.member != None): #funmode return
-            if(info.msg == u'I need some color see see!'):
-                return "@img@./Image/yellow.png"
-            else:
-                return
-        else:
-            return info.msg; #workmode return
+        if(info.msg == "dpdp"):
+            SWITCH = 1
+            return "I had an zhong see you a long time!"
+        elif(info.msg == "stop"):
+            SWITCH = 0
+            return "I miss you very much!"
+        elif(info.msg == "help"):
+            return """#when say "dpdp" start to work
+#when say "help" repaly what is the usage
+#when say "stop" stop fxxking
+#fun mode(1):when say "have fun" to change to fun mode
+#linux shell mode(2):when say "work a moment" to change to shell mode 
+"""
+        elif(SWITCH == 1 or SWITCH == 2 or SWITCH == 3):
+            #choose mode
+            if(info.msg == "have fun"): #funmode return
+                SWITCH = 2
+                return "fxxking"
+            elif(info.msg == "work a moment"):
+                SWITCH = 3
+                return "woking"
+
+        #default cmd
+        if(info.msg == u'I need some color see see!'):
+            return "@img@./Image/yellow.png"
+        if(SWITCH == 2):
+            return
+        elif(SWITCH == 3):
+            cmd = info.msg + ">.tmp"
+            os.system(cmd)
+            remsg = parserfile(".tmp")
+            return remsg
+            #return info.msg; #workmode return
+            
     #info.msg
 
 #working mode
 @bot.register(bot.file_helper,None,False)
 def print_messages(msg):
-       print(msg)
-       newmsg = parsermsg(msg)
-       if (newmsg == None):
-           return
-       #cmd = newmsg + ">.tmp"
-       #os.system(cmd)
-       #remsg = parserfile(".tmp")
-       #bot.file_helper.send(remsg)
-       bot.file_helper.send(newmsg)
-       return
+    global SWITCH
+    print(msg)
+    newmsg = parsermsg(msg)
+    if (newmsg == None):
+        return
+    bot.file_helper.send(newmsg)
+    return
 
 #fun mode
 @bot.register(my_group,None,False)
 def cmdParser(msg):
-    #if not msg.is_at:
-    #    return
-    #else:       
-        print msg
-        newmsg = parsermsg(msg)
-        if (newmsg == None):
-            return
-        print newmsg
-        return newmsg
+    global SWITCH
+    print msg
+    newmsg = parsermsg(msg)
+    if (newmsg == None):
+        return
+    return newmsg
+    
 
 #embed()
 listen(bot)
